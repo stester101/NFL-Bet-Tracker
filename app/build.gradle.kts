12ca85de93bc74176a58b3a30b3e3a74ad5a8391
stester101 <85431356+stester101@users.noreply.github.com>
2026-09-13 19:37:25 +0100

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "uk.co.simon.nflbettracker"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "uk.co.simon.nflbettracker"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
